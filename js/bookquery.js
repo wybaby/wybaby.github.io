7133ca40c6c4978a706d249a074f172866c3c396
wybaby <wybaby@gmail.com>
2017-06-09 15:20:22 +0800

    fix

--- js/bookquery.js
+++ js/bookquery.js
@@ -18,6 +18,7 @@ Papa.parse("https://wybaby.github.io/book/book.txt", {
 //            console.log(b[3]);
             if (b[3].indexOf(searchname)>=0) {
                 found = true;
+				$tblStr += "<tr><td class='table-x'>" + "序号" + "</td><td>" + b[0] + "</td></tr>";
                 $tblStr += "<tr><td class='table-x'>" + "编号" + "</td><td>" + b[1] + "</td></tr>";
                 $tblStr += "<tr><td class='table-x'>" + "书名" + "</td><td>" + b[3] 				
 				if (b[4].length>0) {
@@ -29,7 +30,12 @@ Papa.parse("https://wybaby.github.io/book/book.txt", {
                 $tblStr += "<tr><td class='table-x'>" + "状态" + "</td><td>" + b[2] + "</td></tr>";
                 $tblStr += "<tr><td class='table-x'>" + "出版日期" + "</td><td>" + b[7] + "</td></tr>";
                 $tblStr += "<tr><td class='table-x'>" + "购买日期" + "</td><td>" + b[16] + "</td></tr>";
-                $tblStr += "<tr><td class='table-x'>" + "阅读日期" + "</td><td>" + b[17] + "</td></tr>";
+				if (b[17].length>0) {
+					$tblStr += "<tr><td class='table-x'>" + "阅读日期" + "</td><td>" + b[17] + "</td></tr>";
+				};
+				if (b[18].length>0) {
+					$tblStr += "<tr><td class='table-x'>" + "阅读日期" + "</td><td>" + b[18] + "</td></tr>";
+				};
                 $tblStr += "<tr><td>----------</td></tr>";
             }
         }
@@ -45,21 +51,4 @@ Papa.parse("https://wybaby.github.io/book/book.txt", {
 
 }
 
-
-function query(){
-//    document.write("right");
-//var ht="<html><script type=\"text/javascript\" src=\"js/bookquery.js\"></script><input type=\"text\" name=\"bookname\" class=\"box\"  id=\"searched_content\" title=\"书名\" />\n<input type=\"submit\" value=\"Go\" class=\"button\" title=\"gogogo\" onclick=\"query()\" /></html>";
-    document.write("left");
-    var fs = require("d3");
-    document.write("right");
-    var iconv = require('iconv-lite');
-
-    data = fs.readFileSync('./book.csv');
-    var buffer = iconv.decode(data,'gbk'); //从gbk解码
-    var string = buffer.toString();
-    var line_list = string.split('\r\n');
-    for (var i in line_list){
-        var item_list = line_list[i].split(','); //这就是一行的每一项组成的列表。
-    }
-
-}
+function trimStr(str){return str.replace(/(^\s*)|(\s*$)/g,"");}
